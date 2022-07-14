f46a623c6ebbc48c8156c725a9f17c0087a0c8f8
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-14 19:17:28 +0200

    feat: routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { Outlet } from 'react-router-dom';
+
 const App = () => {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+  return <Outlet />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import Dashboard from './pages/dashboard';
+
+const Container = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>
 );
