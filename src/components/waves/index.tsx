bce8cd72a20695faf0661d1a99f2158d55a6de54
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-15 11:51:51 +0200

    styles: waves component created and styles added

---
+++ src/components/waves/index.tsx
@@ -0,0 +1,41 @@
+const Waves = () => {
+  return (
+    <section>
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#309be4ff"
+          fillOpacity="0.2"
+          d="M0,160L120,138.7C240,117,480,75,720,58.7C960,43,1200,53,1320,58.7L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+        ></path>
+      </svg>
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#309be4ff"
+          fillOpacity="0.2"
+          d="M0,320L80,293.3C160,267,320,213,480,192C640,171,800,181,960,160C1120,139,1280,85,1360,58.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+        ></path>
+      </svg>
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#65b12bff"
+          fillOpacity="0.2"
+          d="M0,64L120,80C240,96,480,128,720,144C960,160,1200,160,1320,160L1440,160L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+  );
+};
+
+export default Waves;
