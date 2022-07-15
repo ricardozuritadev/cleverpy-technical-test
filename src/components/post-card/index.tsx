83791fccc1e70ba0c96662e2cdd5572d05973194
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-15 08:46:02 +0200

    chore: user and post cards components added

---
+++ src/components/post-card/index.tsx
@@ -0,0 +1,19 @@
+const PostCard = () => {
+  return (
+    <section>
+      <div>
+        <h3>Post title</h3>
+        <p>...</p>
+      </div>
+      <div>
+        <p>
+          uia et suscipit\nsuscipit recusandae consequuntur expedita et
+          cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
+          sunt rem eveniet architecto
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PostCard;
---
+++ src/components/user-card/index.tsx
@@ -0,0 +1,12 @@
+const UserCard = () => {
+  return (
+    <section>
+      <picture>
+        <img src="" alt="" />
+      </picture>
+      <h3>User 1</h3>
+    </section>
+  );
+};
+
+export default UserCard;
