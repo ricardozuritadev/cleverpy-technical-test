e93b5bf7d800d289a0599967588805f3a884b00a
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-15 11:52:30 +0200

    styles: auth styles added

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,5 +1,57 @@
-const Login = () => {
-  return <div>Login</div>;
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import images from '../../../utils/imgLoader';
+
+import Input from '../../../components/input';
+import Button from '../../../components/button';
+import Waves from '../../../components/waves';
+
+type UserData = {
+  email: string;
+  password: string;
+};
+
+const Login: React.FC = () => {
+  const submitRef = useRef<any>(null);
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const handleClick = () => navigate('/register');
+  const handleButton = () => submitRef.current.click();
+  const onSubmit = (data: any) => {
+    console.info('> user data: ', data);
+  };
+
+  return (
+    <section className="auth">
+      <div className="auth__pic">
+        <img className="auth__img" src={images.avatar} alt="" />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="auth__form">
+        <Input type="email" placeholder="email" fn={() => register('email')} />
+        <Input
+          type="password"
+          placeholder="password"
+          fn={() => register('password')}
+        />
+        <input type="submit" ref={submitRef} style={{ display: 'none' }} />
+      </form>
+
+      <section className="auth__buttons">
+        <Button text="Login" handleClick={handleButton} />
+        <p className="auth__text">
+          Don't have an account?{' '}
+          <span onClick={handleClick} className="auth__signup">
+            Signup here
+          </span>
+        </p>
+      </section>
+
+      <Waves />
+    </section>
+  );
 };
 
 export default Login;
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,5 +1,62 @@
-const Register = () => {
-  return <div>Register</div>;
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import images from '../../../utils/imgLoader';
+
+import Input from '../../../components/input';
+import Button from '../../../components/button';
+import Waves from '../../../components/waves';
+
+type UserData = {
+  email: string;
+  password: string;
+};
+
+const Register: React.FC = () => {
+  const submitRef = useRef<any>(null);
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const handleClick = () => navigate('/login');
+  const handleButton = () => submitRef.current.click();
+  const onSubmit = (data: any) => {
+    console.info('> user data: ', data);
+  };
+
+  return (
+    <section className="auth">
+      <div className="auth__pic">
+        <img className="auth__img" src={images.avatar} alt="" />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="auth__form">
+        <Input type="email" placeholder="email" fn={() => register('email')} />
+        <Input
+          type="username"
+          placeholder="username"
+          fn={() => register('username')}
+        />
+        <Input
+          type="password"
+          placeholder="password"
+          fn={() => register('password')}
+        />
+        <input type="submit" ref={submitRef} style={{ display: 'none' }} />
+      </form>
+
+      <section className="auth__buttons">
+        <Button text="Signup" handleClick={handleButton} />
+        <p className="auth__text">
+          Already have an account?{' '}
+          <span onClick={handleClick} className="auth__signup">
+            Login here
+          </span>
+        </p>
+      </section>
+
+      <Waves />
+    </section>
+  );
 };
 
 export default Register;
