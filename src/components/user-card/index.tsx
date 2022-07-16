17cac376073d2cf7a889c416032221d391cc4300
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-16 21:05:47 +0200

    styles: user avatar added

--- src/components/user-card/index.tsx
+++ src/components/user-card/index.tsx
@@ -1,9 +1,11 @@
+import images from '../../utils/imgLoader';
+
 const UserCard = () => {
   return (
-    <section>
-      <picture>
-        <img src="" alt="" />
-      </picture>
+    <section className="user card">
+      <section>
+        <img src={images.avatar} alt="user-pic" />
+      </section>
       <h3>User 1</h3>
     </section>
   );
