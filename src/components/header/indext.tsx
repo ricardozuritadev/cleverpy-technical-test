49cbe9860f53677e9ad146aabfed8ebbdbde4928
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-16 20:02:00 +0200

    styles: header styles added

--- src/components/header/indext.tsx
+++ src/components/header/indext.tsx
@@ -1,5 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="header__icons" />
+      </div>
+
+      <nav className="header__desktop">Header nav links</nav>
+
+      <div className="header__desktop">
+        <FontAwesomeIcon icon={faRightFromBracket} className="header__icons" />
+      </div>
+
+      <div className="header__mobile">
+        <FontAwesomeIcon icon={faBars} className="header__icons" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
