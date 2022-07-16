50b3db35e75a1358526315eb88dcffed99f8ed06
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-16 20:46:06 +0200

    styles: manage component styles added

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,5 +1,37 @@
+import PostCard from '../../components/post-card';
+import UserCard from '../../components/user-card';
+
 const Manage = () => {
-  return <div>Manage</div>;
+  return (
+    <section className="manage dashboard__container">
+      <h2 className="heading__secondary">
+        Select a user to manage their posts
+      </h2>
+      <section>
+        <h3>Users</h3>
+        <section className="manage__users">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </section>
+      </section>
+      <section>
+        <h3>Posts</h3>
+        <section className="manage__posts">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default Manage;
