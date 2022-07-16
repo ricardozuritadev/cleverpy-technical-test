b50519c79f711f0b84e5937d12f3284969c97607
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-16 17:44:03 +0200

    chore: dashboard component implemented

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,32 @@
+import PostCard from '../../components/post-card';
+import UserCard from '../../components/user-card';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <section className="dashboard">
+      <nav className="dashboard__nav"></nav>
+
+      <section className="dashboard__section">
+        <h2 className="dashboard__title">
+          Select a user to manage their posts
+        </h2>
+        <section className="dashboard__data">
+          <section className="dashboard__container">
+            <h3 className="dashboard__subtitle">Users</h3>
+            <div className="dashboard__cards">
+              <UserCard />
+            </div>
+          </section>
+          <section className="dashboard__container">
+            <h3 className="dashboard__subtitle">Posts</h3>
+            <div className="dashboard__cards">
+              <PostCard />
+            </div>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default Dashboard;
