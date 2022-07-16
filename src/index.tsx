d777ce0c5f38148783dd4e8494d380c38db93877
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-16 18:09:33 +0200

    chore: dashboard routes added

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,20 @@ import App from './App';
 import Login from './pages/auth/login';
 import Register from './pages/auth/register';
 import Dashboard from './pages/dashboard';
+import Manage from './pages/manage';
+import Users from './pages/users';
+import Posts from './pages/posts';
 
 const Container = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route index element={<Manage />} />
+            <Route path="users" element={<Users />} />
+            <Route path="posts" element={<Posts />} />
+          </Route>
         </Route>
 
         <Route path="/login" element={<Login />} />
