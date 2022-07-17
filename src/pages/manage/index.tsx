9ad22f0db34363bd748adc9ae655eede0a6a383b
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-17 21:17:58 +0200

    chore: manage component modified

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,37 +1,41 @@
 import { useGetter } from '../../context';
+import { useParams } from 'react-router-dom';
 
 import PostCard from '../../components/post-card';
 import UserCard from '../../components/user-card';
 
 const Manage = () => {
-  const { posts } = useGetter();
+  const { users, posts } = useGetter();
+  const { idUser } = useParams();
+
+  const filteredPosts = posts.filter(
+    ({ userId }: any) => userId === Number(idUser)
+  );
 
   return (
     <section className="manage dashboard__container">
-      <h2 className="heading__secondary">
-        Select a user to manage their posts
-      </h2>
       <section className="manage__section">
         <section>
           <h3>Users</h3>
           <section className="manage__cards">
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
-            <UserCard />
+            {users.map((user: any) => (
+              <UserCard key={user.id} {...user} />
+            ))}
           </section>
         </section>
 
         <section>
           <h3>Posts</h3>
           <section className="manage__cards manage__cards--posts">
-            {posts.map((post: any) => (
-              <PostCard key={post.id} {...post} />
-            ))}
+            {idUser ? (
+              filteredPosts.map((post: any) => (
+                <PostCard key={post.id} {...post} />
+              ))
+            ) : (
+              <h2 className="heading__secondary manage__empty">
+                Select a user to manage their posts
+              </h2>
+            )}
           </section>
         </section>
       </section>
