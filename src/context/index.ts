f66c8b0104db1426fdf71aaa4a634c3e439911de
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-17 21:15:41 +0200

    chore: context types added

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,18 +1,12 @@
 import { createContext, useContext } from 'react';
+import { ContextTypes } from './types';
 
-type AppContextInterface = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-};
-
-const Context: any = createContext<AppContextInterface | null>(null);
+const Context: any = createContext<ContextTypes | null>(null);
 
 export const useGetter = () => {
-  const { posts, setPosts } = useContext(Context);
+  const { users, setUsers, posts, setPosts } = useContext(Context);
 
-  return { posts, setPosts };
+  return { users, setUsers, posts, setPosts };
 };
 
 export default Context;
