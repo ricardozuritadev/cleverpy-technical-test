9ef98ecf2f943cd73580de41dac0c7dbf201d1de
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-17 18:31:31 +0200

    feat: real data implemented

--- src/components/post-card/index.tsx
+++ src/components/post-card/index.tsx
@@ -1,16 +1,14 @@
-const PostCard = () => {
+import { PostTypes } from './types';
+
+const PostCard: React.FC<PostTypes> = ({ title, body }) => {
   return (
     <section className="post">
       <div className="post__header">
-        <h3>Post title</h3>
+        <h3>{title}</h3>
         <p>...</p>
       </div>
       <div className="post_content">
-        <p className="post__text">
-          uia et suscipit\nsuscipit recusandae consequuntur expedita et
-          cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
-          sunt rem eveniet architecto
-        </p>
+        <p className="post__text">{body}</p>
       </div>
     </section>
   );
