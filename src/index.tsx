d32db222ca06566feb57180b0238546e16aa57fd
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-17 18:31:06 +0200

    feat: usePosts implemented

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import './styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { usePosts } from './hooks';
+import Context from './context';
 
 import App from './App';
 import Login from './pages/auth/login';
@@ -13,21 +15,25 @@ import Users from './pages/users';
 import Posts from './pages/posts';
 
 const Container = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="dashboard" element={<Dashboard />}>
-            <Route index element={<Manage />} />
-            <Route path="users" element={<Users />} />
-            <Route path="posts" element={<Posts />} />
-          </Route>
-        </Route>
+  const [posts, setPosts] = usePosts();
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+  return (
+    <Context.Provider value={{ posts, setPosts }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="dashboard" element={<Dashboard />}>
+              <Route index element={<Manage />} />
+              <Route path="users" element={<Users />} />
+              <Route path="posts" element={<Posts />} />
+            </Route>
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </Context.Provider>
   );
 };
 
