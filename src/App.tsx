2e6fa30fd1a83ea0251e56c514dd540202b5ee38
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-17 17:52:32 +0200

    feat: context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useGetter } from './context';
 
 const App = () => {
+  const { setPosts } = useGetter();
+
+  useEffect(() => {
+    setPosts();
+  }, []);
+
   return <Outlet />;
 };
 
