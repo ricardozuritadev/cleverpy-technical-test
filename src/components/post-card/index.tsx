b0ec145e718a7068d40af6970c7c47d294964c57
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-18 09:58:30 +0200

    styles: posts cards styles modified

--- src/components/post-card/index.tsx
+++ src/components/post-card/index.tsx
@@ -1,13 +1,16 @@
 import { PostTypes } from './types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
-const PostCard: React.FC<PostTypes> = ({ title, body }) => {
+const PostCard: React.FC<PostTypes> = ({ title, body, user }) => {
   return (
     <section className="post">
       <div className="post__header">
-        <h3>{title}</h3>
-        <p>...</p>
+        <p className="post__author">{user.name}</p>
+        <FontAwesomeIcon icon={faEllipsis} />
       </div>
       <div className="post_content">
+        <h4 className="post__title">{title}</h4>
         <p className="post__text">{body}</p>
       </div>
     </section>
