a191442973bb5408557012faae61634ccc7d7bd1
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-18 15:07:21 +0200

    chore: manage component modified

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useGetter } from '../../context';
 import { useParams } from 'react-router-dom';
 
@@ -7,12 +8,21 @@ import UserCard from '../../components/user-card';
 const Manage = () => {
   const { users, posts } = useGetter();
   const { idUser } = useParams();
-
-  const filteredPosts = posts.filter(
-    ({ userId }: any) => userId === Number(idUser)
-  );
+  const [userPosts, setUserPosts] = useState<any>([]);
 
   const filteredUser = users.find(({ id }: any) => id === Number(idUser));
+
+  useEffect(() => {
+    const filteredPosts = posts.filter(
+      ({ userId }: any) => userId === Number(idUser)
+    );
+    setUserPosts(filteredPosts);
+  }, [idUser]);
+
+  const handleDelete = (postId: number) => {
+    const result = userPosts.filter(({ id }: any) => id !== postId);
+    setUserPosts(result);
+  };
 
   return (
     <section className="manage dashboard__container">
@@ -28,11 +38,23 @@ const Manage = () => {
 
         <section>
           <h3 className="manage__title">Posts</h3>
+
           <section className="manage__cards manage__cards--posts">
             {idUser ? (
-              filteredPosts.map((post: any) => (
-                <PostCard key={post.id} {...post} user={filteredUser} />
-              ))
+              userPosts.length > 0 ? (
+                userPosts.map((post: any) => (
+                  <PostCard
+                    key={post.id}
+                    {...post}
+                    user={filteredUser}
+                    handleDelete={handleDelete}
+                  />
+                ))
+              ) : (
+                <h2 className="heading__secondary manage__empty">
+                  {filteredUser.name} has no posts
+                </h2>
+              )
             ) : (
               <h2 className="heading__secondary manage__empty">
                 Select a user to manage their posts
