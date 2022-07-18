6b420ec99c7c7b398b437ef06e3a789cc235fe37
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-18 19:35:22 +0200

    chore: all users page created

--- src/pages/all-users/index.tsx
+++ src/pages/all-users/index.tsx
@@ -1,5 +1,21 @@
+import { UserTypes } from './types/UserTypes';
+import { useGetter } from '../../context';
+
+import UserInfo from '../../components/user-info';
+
 const Users = () => {
-  return <div>Users</div>;
+  const { users } = useGetter();
+
+  return (
+    <section className="users">
+      <h2 className="users__title">Users</h2>
+      <section className="users__grid">
+        {users.map((user: UserTypes) => (
+          <UserInfo key={user.id} {...user} />
+        ))}
+      </section>
+    </section>
+  );
 };
 
 export default Users;
