7f922ad3d78b4a23ed10cc21f092749de10bdc1f
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-18 17:09:04 +0200

    chore: dropdown component and styles modified

--- src/components/dropdown-menu/index.tsx
+++ src/components/dropdown-menu/index.tsx
@@ -1,23 +1,72 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
 import { DropdownProps } from './types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
-const DropdownMenu: React.FC<DropdownProps> = ({ postId, handleDelete }) => {
+const MySwal = withReactContent(Swal);
+
+const DropdownMenu: React.FC<DropdownProps> = ({
+  postId,
+  handleDelete,
+  setEditedText,
+}) => {
   const [isHide, setIsHide] = useState<boolean>(true);
 
-  const handleClick = () => setIsHide(!isHide);
+  const handleShowHide = () => setIsHide(!isHide);
+
+  const handleEdit = async () => {
+    handleShowHide();
+    return MySwal.fire({
+      title: 'Enter new post text',
+      input: 'textarea',
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      confirmButtonColor: '#65b12bff',
+      cancelButtonText: 'Cancel',
+      cancelButtonColor: '#e60023ff',
+      width: '50rem',
+    }).then(postText => {
+      if (postText.value) {
+        setEditedText(postText.value);
+      }
+    });
+  };
+
+  const handleDeletePost = async () => {
+    handleShowHide();
+    return MySwal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#65b12bff',
+      cancelButtonColor: '#e60023ff',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        handleDelete(postId);
+      }
+    });
+  };
 
   return (
     <div className="dropdown">
-      <button className="dropdown__btn" onClick={handleClick}>
-        <FontAwesomeIcon icon={faEllipsis} className="post__dots" />
+      <button className="dropdown__btn" onClick={handleShowHide}>
+        <FontAwesomeIcon icon={faEllipsis} className="dropdown__dots" />
       </button>
       {!isHide && (
         <section className="dropdown__options">
-          <button className="dropdown__text dropdown__text--green">Edit</button>
           <button
-            onClick={() => handleDelete(postId)}
+            onClick={handleEdit}
+            className="dropdown__text dropdown__text--green"
+          >
+            Edit
+          </button>
+          <button
+            onClick={handleDeletePost}
             className="dropdown__text dropdown__text--red"
           >
             Delete
