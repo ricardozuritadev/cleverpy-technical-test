307410736e5453666e257cd32b128e1cb44f6d1e
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 11:20:26 +0200

    feat: searchbar component and styles modified

--- src/components/header/indext.tsx
+++ src/components/header/indext.tsx
@@ -2,17 +2,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useGetter } from '../../context';
 
 const Header = () => {
+  const { setSearch } = useGetter();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <header className="header">
-      <div>
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="header__icons" />
+      <div className="header__searchbar">
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Search user"
+          className="header__input"
+        />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="header__icons header__icons--search"
+        />
       </div>
 
-      <nav className="header__desktop">Header nav links</nav>
-
       <div className="header__desktop">
+        <span style={{ paddingRight: '1rem' }}>Logout</span>
         <FontAwesomeIcon icon={faRightFromBracket} className="header__icons" />
       </div>
 
