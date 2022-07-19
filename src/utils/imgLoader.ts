bd8a93a6472857cc31c36bd9f6c754132834a4ab
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 20:35:45 +0200

    chore: imageLoader types added

--- src/utils/imgLoader.ts
+++ src/utils/imgLoader.ts
@@ -1,7 +1,11 @@
 import avatar from '../assets/images/avatar.png';
 
+type Images = {
+  avatar: string;
+};
+
 // Función para importar imágenes donde las necesite
-const images = {
+const images: Images = {
   avatar,
 };
 
