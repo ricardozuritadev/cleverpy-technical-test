3172fea3b7fa06a872a8b74682dbde0426c33e93
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 14:00:31 +0200

    feat: logout button functionality implemented

--- src/components/header/indext.tsx
+++ src/components/header/indext.tsx
@@ -1,14 +1,37 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useGetter } from '../../context';
 
+const MySwal = withReactContent(Swal);
+
 const Header = () => {
-  const { search, setSearch } = useGetter();
+  const { search, setSearch, setAdmin } = useGetter();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
+  };
+
+  const logout = () => {
+    return MySwal.fire({
+      title: 'Do you want to leave?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#65b12bff',
+      cancelButtonColor: '#e60023ff',
+      confirmButtonText: 'Yes, logout!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        setAdmin(null);
+        return MySwal.fire({
+          text: 'Good bye!',
+          confirmButtonColor: '#65b12bff',
+        });
+      }
+    });
   };
 
   return (
@@ -27,7 +50,7 @@ const Header = () => {
         />
       </div>
 
-      <div className="header__desktop">
+      <div className="header__desktop" onClick={logout}>
         <span style={{ paddingRight: '1rem' }}>Logout</span>
         <FontAwesomeIcon icon={faRightFromBracket} className="header__icons" />
       </div>
