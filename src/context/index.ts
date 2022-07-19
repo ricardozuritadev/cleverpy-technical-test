d6a996f08232ece7ad9aab5d8677975a01bc3e09
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 11:20:46 +0200

    feat: searchbar context implemented

--- src/context/index.ts
+++ src/context/index.ts
@@ -4,9 +4,10 @@ import { ContextTypes } from './types';
 const Context: any = createContext<ContextTypes | null>(null);
 
 export const useGetter = () => {
-  const { users, setUsers, posts, setPosts } = useContext(Context);
+  const { users, setUsers, posts, setPosts, search, setSearch } =
+    useContext(Context);
 
-  return { users, setUsers, posts, setPosts };
+  return { users, setUsers, posts, setPosts, search, setSearch };
 };
 
 export default Context;
