49e1f5ff19bc9c3730fda72255d9f25809c6f226
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 11:52:23 +0200

    chore: not found page created

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,12 @@
+import Waves from '../../components/waves';
+
+const NotFound = () => {
+  return (
+    <section className="auth">
+      <h2>404 - Page not found</h2>
+      <Waves />
+    </section>
+  );
+};
+
+export default NotFound;
