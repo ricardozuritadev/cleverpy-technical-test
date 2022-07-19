5b201120310bf6389e3bc83fa850e95c64e08ec3
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-19 16:42:59 +0200

    chore: admin profile page created

---
+++ src/pages/admin-profile/index.tsx
@@ -0,0 +1,24 @@
+import { useGetter } from '../../context';
+import images from '../../utils/imgLoader';
+
+const AdminProfile = () => {
+  const { admin } = useGetter();
+
+  return (
+    <section className="admin">
+      <h2 className="heading__secondary">My profile</h2>
+      <section className="admin__card">
+        <section className="admin__avatar">
+          <img src={images.avatar} alt="user-avatar" className="admin__img" />
+        </section>
+        <section className="admin__profile">
+          <p className="admin__username">{admin.username}</p>
+          <p className="admin__email">{admin.email}</p>
+          <p className="admin__role">Site admin</p>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default AdminProfile;
