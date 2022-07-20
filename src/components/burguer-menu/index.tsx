b0938d76bcfab1166cfa8a51c7931e9c37db8f00
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-20 12:53:42 +0200

    feat: burguer menu translations implemented

--- src/components/burguer-menu/index.tsx
+++ src/components/burguer-menu/index.tsx
@@ -2,9 +2,14 @@ import { slide as Menu } from 'react-burger-menu';
 import { NavLink } from 'react-router-dom';
 import { useGetter } from '../../context';
 import images from '../../utils/imgLoader';
+import { useTranslation } from 'react-i18next';
+
+import LangSelector from '../lang-selector';
 
 const BurguerMenu = () => {
   const { admin } = useGetter();
+
+  const { t } = useTranslation();
 
   return (
     <Menu right>
@@ -12,23 +17,25 @@ const BurguerMenu = () => {
         <div className="aside__avatar">
           <img className="aside__img" src={images.avatar} alt="user-avatar" />
         </div>
-        <p className="aside__welcome">Welcome</p>
+        <p className="aside__welcome">{t('welcome')}</p>
         <p className="aside__username">{admin.username}</p>
-        <p className="aside__admin">Site admin</p>
+        <p className="aside__admin">{t('role')}</p>
       </section>
 
       <NavLink id="home" className="menu-item" to="/dashboard/manage">
-        Manage posts
+        {t('manage')}
       </NavLink>
       <NavLink id="about" className="menu-item" to="/dashboard/users">
-        Users
+        {t('users')}
       </NavLink>
       <NavLink id="contact" className="menu-item" to="/dashboard/profile">
-        My profile
+        {t('profile')}
       </NavLink>
       <NavLink className="menu-item" to="/login">
-        Logout
+        {t('logout')}
       </NavLink>
+
+      <LangSelector />
     </Menu>
   );
 };
