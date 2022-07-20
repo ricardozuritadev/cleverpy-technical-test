5a165beefaca8795230b953aaccd28bf208da734
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-20 12:55:23 +0200

    feat: admin profile translations implemented

--- src/pages/admin-profile/index.tsx
+++ src/pages/admin-profile/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { useGetter } from '../../context';
 import images from '../../utils/imgLoader';
 
@@ -5,9 +6,11 @@ const AdminProfile = () => {
   // Me traigo el admin del contexto con el hook useGetter()
   const { admin } = useGetter();
 
+  const { t } = useTranslation();
+
   return (
     <section className="admin">
-      <h2 className="heading__secondary">My profile</h2>
+      <h2 className="heading__secondary">{t('profile')}</h2>
       <section className="admin__card">
         <section className="admin__avatar">
           <img src={images.avatar} alt="user-avatar" className="admin__img" />
@@ -15,7 +18,7 @@ const AdminProfile = () => {
         <section className="admin__profile">
           <p className="admin__username">{admin.username}</p>
           <p className="admin__email">{admin.email}</p>
-          <p className="admin__role">Site admin</p>
+          <p className="admin__role">{t('role')}</p>
         </section>
       </section>
     </section>
