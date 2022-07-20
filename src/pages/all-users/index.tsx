b380cdf320bd18fa33bd61d57a56ad3a41e7303e
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-20 12:55:37 +0200

    feat: all users translations implemented

--- src/pages/all-users/index.tsx
+++ src/pages/all-users/index.tsx
@@ -1,11 +1,14 @@
 import { UserTypes } from './types/UserTypes';
 import { useGetter } from '../../context';
+import { useTranslation } from 'react-i18next';
 
 import UserInfo from '../../components/user-info';
 
 const Users = () => {
   // Me traigo los usuarios y el texto de búsqueda del contexto
   const { users, search } = useGetter();
+
+  const { t } = useTranslation();
 
   // Filtro los usuarios cuando se usa el buscador
   const filteredUsers = users.filter((user: UserTypes) => {
@@ -18,14 +21,14 @@ const Users = () => {
 
   return (
     <section className="users">
-      <h2 className="users__title">Users</h2>
+      <h2 className="users__title">{t('users')}</h2>
       <section className="users__grid">
         {filteredUsers.length > 0 ? (
           filteredUsers.map((user: UserTypes) => (
             <UserInfo key={user.id} {...user} />
           ))
         ) : (
-          <h2>No user found</h2>
+          <h2>{t('no_user')}</h2>
         )}
       </section>
     </section>
