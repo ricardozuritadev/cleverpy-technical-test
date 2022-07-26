3dddec35c7156647b7ad4b53a2ee5b0d765b6d2f
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-26 14:15:01 +0200

    feat: posts reducer created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { postsSlice } from './slices/posts';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postsSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
