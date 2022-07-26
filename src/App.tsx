82683b35e547c6e87714cbf4027ac87d856e23e5
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-26 20:00:27 +0200

    feat: redux implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useGetter } from './context';
+import { useAppDispatch } from './store/hooks';
+import { loadingPosts, setPosts } from './store/slices/posts';
+import { loadingUsers, setUsers } from './store/slices/users';
+import services from './services';
 
 const App = () => {
-  const { setUsers, setPosts } = useGetter();
+  const dispatch = useAppDispatch();
+
+  // Llamo a mis servicios para traerme posts y users
+  const getSampleData = async () => {
+    const postsData = await services.sampleData.getPosts();
+    const usersData = await services.sampleData.getUsers();
+    dispatch(loadingPosts());
+    dispatch(setPosts(postsData));
+    dispatch(loadingUsers());
+    dispatch(setUsers(usersData));
+  };
 
   // Traigo los usuarios y los posts cuando se renderiza el componente App
-  // utilizando el hook useGetter()
   useEffect(() => {
-    setUsers();
-    setPosts();
+    getSampleData();
   }, []);
 
   return <Outlet />;
